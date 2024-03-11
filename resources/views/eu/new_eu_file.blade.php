f80a026e95685b288a15e14d90a0be978063920e
MinaFouad146 <57178389+MinaFouad146@users.noreply.github.com>
2024-03-11 13:51:34 +0200

    the main template

---
+++ resources/views/eu/new_eu_file.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+
+@section('title')
+@endsection
+
+@section('contentHeader')
+@endsection
+
+
+{{-- ################################## breadcrumb-item ################## --}}
+
+@section('contentHeaderLink')
+@endsection
+
+@section('contentHeaderActive')
+@endsection
+
+{{-- ################################## Content ################## --}}
+@section('backgroundimg')
+    {{-- {{ asset('assets/images/prints.png') }} --}}
+@endsection
+
+@section('content')
+@endsection
+
+
+
+
+{{-- ################################## Styles & Scripts ################## --}}
+@section('styles')
+@endsection
+
+
+@section('scripts')
+@endsection
